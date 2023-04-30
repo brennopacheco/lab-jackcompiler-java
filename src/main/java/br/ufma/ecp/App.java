dc53320935208e3132d21f7d9a4655620c5ade90
Brenno Pacheco <brenno_pacheco@hotmail.com>
2023-04-30 14:39:51 -0300

    Reconhecendo símbolos

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -8,7 +8,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        String input = "45 variavel while if";
+        String input = "45 variavel + while < , if";
         Scanner scan = new Scanner(input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != EOF; tk = scan.nextToken()) {
             System.out.println(tk);
